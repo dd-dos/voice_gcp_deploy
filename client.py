06d2e3f3539297c41bf3cfde9b2a93b06f0264fe
dd-dos <dungphamdang99@gmail.com>
2020-10-26 10:22:15 +0700

    new commit

--- client.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from mlchain.client import Client
-
-model = Client(api_address='http://localhost:5000').model()
-wf = '/home/pdd/Desktop/sandbox/AI_interview/sample_wav/sample.wav'
-result = model.get_reaction_score(wf)
-print(result)  
